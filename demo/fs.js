dc93b0dd34a528d0e5682af146b6ed21fc23ac24
Jason8558 <71411694+Jason8558@users.noreply.github.com>
2020-10-08 09:06:12 +1200

    Add files from local storage

---
+++ demo/fs.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// fs.mkdir(path.join(__dirname, 'test'), (err) => {
+//   if (err) {
+//     throw err
+//   }
+//
+//   console.log('Папка создана')
+// })
+
+const filePath = path.join(__dirname, 'test', 'text.txt')
+// 
+// fs.writeFile(filePath, 'Hello NodeJS!', err => {
+//   if (err) {
+//     throw err
+//   }
+//
+//   console.log('Файл создан');
+// })
+
+fs.readFile(filePath, (err) => {
+  if (err) {
+    throw err
+  }
+
+const data = Buffer.from()
+
+})
